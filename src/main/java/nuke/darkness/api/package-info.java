d84fd720c392e72895c16fbc558df8013b9ed53a
Jon Marien <nuclearsmilz@gmail.com>
2017-06-19 12:37:01 -0400

    Changed how versioning works and added api package, prep to add rune api

---
+++ src/main/java/nuke/darkness/api/package-info.java
@@ -0,0 +1,4 @@
+@API(owner = nuke.darkness.client.util.References.MODID, provides = nuke.darkness.client.util.References.MODID + ".api", apiVersion = nuke.darkness.client.util.References.APIVERSION)
+package nuke.darkness.api;
+
+import net.minecraftforge.fml.common.*;
--- src/main/java/nuke/darkness/client/util/References.java
+++ src/main/java/nuke/darkness/client/util/References.java
@@ -7,25 +7,11 @@ import org.apache.logging.log4j.*;
 public class References {
 
 	public static final String MCVERSION = "1.11.2";
-	/**
-	 * Updates every MAJOR change,
-	 * never resets
-	 */
-	public static final int MAJOR = 0;
-	/**
-	 * Updates every time a new block, item or features is added or change,
-	 * resets on MAJOR changes
-	 */
-	public static final int MINOR = 0;
-	/**
-	 * Updates every time a bug is fixed or issue solved or very minor code changes,
-	 * resets on MINOR changes
-	 */
-	public static final int PATCH = 0;
 
 	public static final String MODID = "darkness";
 	public static final String NAME = "Darkness";
-	public static final String VERSION = MCVERSION + "-" + MAJOR + "." + MINOR + "." + PATCH + "";
+	public static final String VERSION = MCVERSION + "-" + "0.0.1";
+	public static final String APIVERSION = MCVERSION + "-" + "1.0.0";
 	public static final String DEPENDENCIES = "";
 	public static final String ACCEPTEDMCVERSIONS = "[1.11.2]";
 
